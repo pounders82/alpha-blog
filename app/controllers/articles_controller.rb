b6bc241fdc68390700a85c86d07739455ffb855c
pounders82 <nickpounders@gmail.com>
2017-07-28 15:53:22 -0400

    Update to Index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,26 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+    end
 
   def create
     #render plain: params[:article].inspect
@@ -26,4 +44,4 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
     end
 
-end
+  end
